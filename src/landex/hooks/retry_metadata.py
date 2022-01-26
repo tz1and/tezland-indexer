ae357762490f2eff0d2097daaa65f910c5693d22
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-01-26 21:32:17 +0100

    added retry_metadata hook/job

---
+++ src/landex/hooks/retry_metadata.py
@@ -0,0 +1,19 @@
+
+from dipdup.context import HookContext
+
+import landex.models as models
+from landex.metadata import get_place_metadata, get_item_metadata
+
+
+async def retry_metadata(
+    ctx: HookContext,
+) -> None:
+    places = await models.PlaceToken.filter(metadata_fetched=False).all()
+    for token in places:
+        await get_place_metadata(token)
+
+    items = await models.ItemToken.filter(metadata_fetched=False).all()
+    for token in items:
+        await get_item_metadata(token)
+    
+    await ctx.execute_sql('retry_metadata')
