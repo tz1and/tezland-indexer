346127409b5124ec5f2db58d7549d3ad0ac09e57
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-04-10 16:36:52 +0200

    run a backup on on synchronised if outdated

--- landex/hooks/on_synchronized.py
+++ landex/hooks/on_synchronized.py
@@ -1,7 +1,39 @@
 
+import logging
+from typing import Union
 from dipdup.context import HookContext
+
+from dipdup.enums import MessageType
+from dipdup.config import PostgresDatabaseConfig, SqliteDatabaseConfig
+import landex.backups as backups
+
+_logger = logging.getLogger(__name__)
 
 async def on_synchronized(
     ctx: HookContext,
 ) -> None:
     await ctx.execute_sql('on_synchronized')
+
+    # do a backup if the last one is too old.
+    # let's say 50 blocks.
+
+    database_config: Union[SqliteDatabaseConfig, PostgresDatabaseConfig] = ctx.config.database
+
+    # if not a postgres db, reindex.
+    if database_config.kind != "postgres":
+        _logger.info('Not postgres database, skipping backup')
+        return
+
+    available_levels = backups.get_available_backups()
+
+    # find highest level
+    highest_level = 0
+    for level in available_levels:
+        if level > highest_level:
+            highest_level = level
+    
+    level = ctx.get_tzkt_datasource("tzkt_mainnet")._level.get(MessageType.head)
+
+    # if highest level is old, run a backup.
+    if level - highest_level > 50:
+        backups.backup(level, database_config)
