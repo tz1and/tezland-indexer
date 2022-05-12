f4f306652dd066eafc090e892453cdac51f418a0
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-05-12 21:00:55 +0200

    Update to 5.1.0, use on_index_rollback.py

--- landex/hooks/on_index_rollback.py
+++ landex/hooks/on_index_rollback.py
@@ -1,29 +1,39 @@
 import logging
 from typing import Union
 
-from dipdup.datasources.datasource import Datasource
-from dipdup.context import HookContext
 from dipdup.enums import ReindexingReason
+from dipdup.index import Index
+from dipdup.context import HookContext
+
 from dipdup.config import PostgresDatabaseConfig, SqliteDatabaseConfig
 
 import landex.backups as backups
 
 _logger = logging.getLogger(__name__)
 
-async def on_rollback(
+async def on_index_rollback(
     ctx: HookContext,
-    datasource: Datasource,
+    index: Index,
     from_level: int,
     to_level: int,
 ) -> None:
-    await ctx.execute_sql('on_rollback')
+    await ctx.execute_sql('on_index_rollback')
+
+    def emitReindex():
+        return ctx.reindex(
+            ReindexingReason.rollback,
+            index=index.name,
+            datasource=index.datasource.name,
+            from_level=from_level,
+            to_level=to_level,
+        )
 
     database_config: Union[SqliteDatabaseConfig, PostgresDatabaseConfig] = ctx.config.database
 
     # if not a postgres db, reindex.
     if database_config.kind != "postgres":
         _logger.info('Not postgres database, skipping restore')
-        await ctx.reindex(ReindexingReason.ROLLBACK)
+        await emitReindex()
         return
 
     available_levels = backups.get_available_backups()
@@ -31,7 +41,7 @@ async def on_rollback(
     # if no backups available, reindex
     if not available_levels:
         _logger.info('Not backups available, skipping restore')
-        await ctx.reindex(ReindexingReason.ROLLBACK)
+        await emitReindex()
         return
 
     # find the right level. ie the on that's closest to to_level
@@ -46,4 +56,4 @@ async def on_rollback(
         _logger.info('Restarting dipdup...')
         await ctx.restart()
     except:
-        await ctx.reindex(ReindexingReason.ROLLBACK)
+        await emitReindex()
