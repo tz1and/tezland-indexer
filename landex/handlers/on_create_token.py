f1b8c5c4c51e4e7660b1d7fd5056e4c2ce58b937
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2023-01-05 20:54:25 +0100

    use code_hash indexing for collections

--- landex/handlers/on_create_token.py
+++ landex/handlers/on_create_token.py
@@ -15,7 +15,6 @@ async def on_create_token(
     origination: OperationData #Origination[TezlandFactoryStorage].data
 ) -> None:
     contract_address = origination.originated_contract_address
-    contract_name = f'itemCollection{contract_address[-12:]}'
 
     # Add the contract.
     await utils.addContract(ctx,
@@ -23,9 +22,3 @@ async def on_create_token(
         owner=origination.initiator_address,
         level=origination.level,
         timestamp=origination.timestamp)
-
-    await ctx.add_contract(contract_name, contract_address, typename='tezlandItemsCollection')
-
-    _logger.info(f'Adding index for item collection {contract_name}')
-    await ctx.add_index(contract_name, 'tezland_item_collection_template',
-        {'contract': contract_name}, first_level=origination.level)
