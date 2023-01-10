b636c6cbf59627dde04883ce372ae9e493975f07
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2023-01-10 17:08:33 +0100

    check if manual reindex was flagged on restart

--- landex/hooks/on_restart.py
+++ landex/hooks/on_restart.py
@@ -1,7 +1,24 @@
+import logging
+from os import remove, path
 
-from dipdup.context import HookContext
+from dipdup.context import HookContext, ReindexingReason
+
+from landex import backups
+
+_logger = logging.getLogger(__name__)
+
 
 async def on_restart(
     ctx: HookContext,
 ) -> None:
     await ctx.execute_sql('on_restart')
+
+    reindex_file = f'{backups.BACKUPS_DIR}/reindex'
+
+    # If the file exists, a reindex is desired.
+    if path.exists(reindex_file) and path.isfile(reindex_file):
+        _logger.info(f'Manual reindex flagged. Reindexing.....')
+        remove(reindex_file)
+        await backups.delete_old_backups(ctx, 0)
+
+        await ctx.reindex(ReindexingReason.config_modified)
--- landex/hooks/trigger_reindex.py
+++ landex/hooks/trigger_reindex.py
@@ -4,4 +4,4 @@ async def trigger_reindex(
     ctx: HookContext
 ) -> None:
     # NOTE: doesn't really work. who knows why. we'll see in prod.
-    await ctx.reindex(ReindexingReason.manual)
+    await ctx.reindex(ReindexingReason.config_modified)
