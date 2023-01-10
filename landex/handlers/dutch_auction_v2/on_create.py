412ccf9e536832027cf88ac897ea9ea9daa83378
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2023-01-10 17:54:10 +0100

    fix for auto increment not working on v2 auctions

--- landex/handlers/dutch_auction_v2/on_create.py
+++ landex/handlers/dutch_auction_v2/on_create.py
@@ -13,8 +13,17 @@ async def on_create(
 ) -> None:
     auction_key = create.parameter.auction_key
     (owner, _) = await models.Holder.get_or_create(address=create.data.sender_address)
+
+    # For some weird reason, autoincrement is not working correctly, so we need to
+    # get the last id and increment it ourselves.... Probably a tortoise bug?
+    last_auction = await models.DutchAuction.all().order_by('-transient_id').first()
+
+    next_id = 0
+    if last_auction is not None:
+        next_id = last_auction.transient_id + 1
     
     auction = models.DutchAuction(
+        transient_id=next_id,
         owner=owner,
         token_id=auction_key.token_id,
         start_price=create.parameter.auction.start_price,
